d1abf8fe1742a7eaecd4a11b94aa60cbda8f44de
Cole <coltonflather@gmail.com>
2024-04-14 18:25:32 -0400

    added Fastify On Server

---
+++ fastifyServer.js
@@ -0,0 +1,101 @@
+// fastify stuff:
+
+const path = require("path");
+
+// Require the fastify framework and instantiate it
+const fastify = require("fastify")({
+  // Set this to true for detailed logging:
+  logger: false,
+});
+
+// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+
+// Setup our static files
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "public"),
+  prefix: "/", // optional: default '/'
+});
+
+// Formbody lets us parse incoming forms
+fastify.register(require("@fastify/formbody"));
+
+// View is a templating manager for fastify
+fastify.register(require("@fastify/view"), {
+  engine: {
+    handlebars: require("handlebars"),
+  },
+});
+
+// Load and parse SEO data
+const seo = require("./src/seo.json");
+if (seo.url === "glitch-default") {
+  seo.url = `https://${process.env.PROJECT_DOMAIN}.glitch.me`;
+}
+
+//end of fastify stuff
+
+const mongoose = require('mongoose');
+const UserModel = require('./userModel');
+
+const connectionString = "mongodb+srv://coltonflather:Wonderful1!@userdatacluster.gskuflb.mongodb.net/"
+mongoose.connect(connectionString);
+
+//post requests
+fastify.post('/add-user', (req, res) => {
+  let body = req.body;
+  const newUser = new UserModel(body);
+
+  newUser.save().then((result) => {
+      res.status(200).send(result);
+  }).catch((error) => {
+      res.status(400).send({ "userExists": false });
+  });
+});
+
+fastify.post('/update-user/:username', (req, res) => {
+  const username = req.params.username;
+  const body = req.body;
+  res.send(body)
+  UserModel.findOneAndUpdate({username: username}, body, {new: true}).exec().then((result)=>{
+    res.status(200);res.send(result)
+  }).catch((error)=>{
+    res.status(400); console.log(error)
+  });
+})
+
+fastify.post('/replace-user/:username', (req, res) => {
+  const username = req.params.username;
+  const body = req.body;
+  res.send(body)
+  UserModel.findOneAndReplace({username: username}, body, {new: true}).exec().then((result)=>{
+    res.status(200);res.send(result)
+  }).catch((error)=>{
+    res.status(400); console.log(error)
+  });
+})
+//puts
+
+fastify.put('/delete-user/:username', (req, res) => {
+  const username = req.params.username;
+  UserModel.findOneAndDelete({username:username}).exec().then((result)=>{
+    res.status(200);res.send(result)
+  }).catch((error)=>{
+    res.status(400); console.log(error)
+  });
+})
+//gets
+fastify.get('/get-user/:username',(req,res)=>{
+    const username = req.params.username
+    UserModel.findOne({username: username}).exec().then((result)=>{
+      res.status(200);res.send(result)
+    }).catch(err=>res.send(err));
+})
+
+//testing
+fastify.post('/', (req, res) => {
+  const body = req.body;
+  res.send(body);
+})
+fastify.get('/', (req, res) => {
+  return {"this worked":true}
+})
